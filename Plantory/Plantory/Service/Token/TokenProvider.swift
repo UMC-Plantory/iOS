8312c8ce8cca549bc7f25ebe0962d5394491623b
minyoy <65756020+minyoy@users.noreply.github.com>
2025-08-01 14:01:14 +0900

    feat: 키체인, 토큰 처리 구현

---
+++ Plantory/Plantory/Service/Token/TokenProvider.swift
@@ -0,0 +1,83 @@
+//
+//  TokenProvider.swift
+//  Plantory
+//
+//  Created by 주민영 on 7/30/25.
+//
+
+import Foundation
+import Moya
+
+class TokenProvider: TokenProviding {
+    private let userSession = "appNameUser"
+    private let keyChain = KeychainService.shared
+    private let provider = MoyaProvider<AuthRouter>()
+    
+    var accessToken: String? {
+        get {
+            guard let userInfo = keyChain.loadToken() else { return nil }
+            return userInfo.accessToken
+        }
+        set {
+            guard var userInfo = keyChain.loadToken() else { return }
+            userInfo.accessToken = newValue ?? "토큰 정보 없음"
+            if keyChain.saveToken(userInfo) {
+                print("유저 액세스 토큰 갱신됨: \(String(describing: newValue))")
+            }
+        }
+    }
+    
+    var refreshToken: String? {
+        get {
+            guard let userInfo = keyChain.loadToken() else { return nil }
+            return userInfo.refreshToken
+        }
+        
+        set {
+            guard var userInfo = keyChain.loadToken() else { return }
+            userInfo.refreshToken = newValue ?? "토큰 정보 없음"
+            if keyChain.saveToken(userInfo) {
+                print("유저 리프레시 갱신됨")
+            }
+        }
+    }
+    
+    func refreshToken(completion: @escaping (String?, (any Error)?) -> Void) {
+        guard let userInfo = keyChain.loadToken() else {
+            let error = NSError(domain: "example.com", code: -2, userInfo: [NSLocalizedDescriptionKey: "UserSession or refreshToken not found"])
+            completion(nil, error)
+            return
+        }
+        let refreshToken = userInfo.refreshToken
+        
+        provider.request(.sendRefreshToken(refreshToken: refreshToken)) { result in
+            switch result {
+            case .success(let response):
+                if let jsonString = String(data: response.data, encoding: .utf8) {
+                    print("응답 JSON: \(jsonString)")
+                } else {
+                    print("JSON 데이터를 문자열로 변환할 수 없습니다.")
+                }
+
+                do {
+                    
+                    let tokenData = try JSONDecoder().decode(TokenInfo.self, from: response.data)
+
+                    
+                    self.accessToken = tokenData.accessToken
+                    self.refreshToken = tokenData.refreshToken
+
+                    completion(self.accessToken, nil)
+                } catch {
+                    print("디코딩 에러: \(error)")
+                    completion(nil, error)
+                }
+
+            case .failure(let error):
+                print("네트워크 에러 : \(error)")
+                completion(nil, error)
+            }
+        }
+    }
+    
+}
