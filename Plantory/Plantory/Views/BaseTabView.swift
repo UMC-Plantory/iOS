74a9def99da9836ad7631f7a04f8d7fa1b12f857
Junghwan <hwane2000@gmail.com>
2025-08-06 18:31:36 +0900

    Merge branch 'main' into 8-feature

--- Plantory/Plantory/Views/BaseTabView.swift
+++ Plantory/Plantory/Views/BaseTabView.swift
@@ -17,78 +17,74 @@ struct BaseTabView: View {
     @State private var isTerrariumPopupVisible = false
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Tab(
-                "",
-                image: selectedTab == .home ? "Home_fill" : "Home",
-                value: TabItem.home
-            ) {
-                HomeView()
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                Tab(
+                    "",
+                    image: selectedTab == .home ? "Home_fill" : "Home",
+                    value: TabItem.home
+                ) {
+                    HomeView()
+                }
+                
+                Tab(
+                    "",
+                    image: selectedTab == .diary ? "Diary_fill" : "Diary",
+                    value: TabItem.diary
+                ) {
+                    DiaryView()
+                }
+                
+                Tab(
+                    "",
+                    image: selectedTab == .terrarium ? "Terrarium_fill" : "Terrarium",
+                    value: TabItem.terrarium
+                ) {
+                    TerrariumView()
+                }
+                
+                Tab(
+                    "",
+                    image: selectedTab == .chat ? "Chat_fill" : "Chat",
+                    value: TabItem.chat
+                ) {
+                    ChatView()
+                }
+                
+                Tab(
+                    "",
+                    image: selectedTab == .profile ? "Profile_fill" : "Profile",
+                    value: TabItem.profile
+                ) {
+                    ProfileView()
+                }
             }
-            
+
             if isTerrariumPopupVisible {
                 TerrariumPopup(isVisible: $isTerrariumPopupVisible)
                     .zIndex(10)
             }
-            
 
-            Tab(
-                "",
-                image: selectedTab == .diary ? "Diary_fill" : "Diary",
-                value: TabItem.diary
-            ) {
-                DiaryView()
-            }
-            
-            Tab(
-                "",
-                image: selectedTab == .terrarium ? "Terrarium_fill" : "Terrarium",
-                value: TabItem.terrarium
-            ) {
-                TerrariumView()
-            }
-            
-            Tab(
-                "",
-                image: selectedTab == .chat ? "Chat_fill" : "Chat",
-                value: TabItem.chat
-            ) {
-                ChatView()
-            }
-            
-            Tab(
-                "",
-                image: selectedTab == .profile ? "Profile_fill" : "Profile",
-                value: TabItem.profile
-            ) {
-                ProfileView()
-            }
-        }
-        .overlay(alignment: .bottom) {
             VStack(spacing: 0) {
                 Divider()
                     .background(Color.gray.opacity(0.4))
                     .frame(height: 1)
-                
                 Spacer().frame(height: 49)
             }
             .allowsHitTesting(false)
         }
-        .navigationTitle("")
-        .navigationBarHidden(true)
-        .onChange(of: selectedTab) { oldValue, newValue in
-            if newValue == .terrarium && !hasShownTerrariumPopup {
-                isTerrariumPopupVisible = true
-                hasShownTerrariumPopup = true
-            }
-        }
-
         .onAppear {
             UITabBar.appearance().backgroundColor = .white01
             UITabBar.appearance().unselectedItemTintColor = .black01
         }
         .ignoresSafeArea(.keyboard)
         .navigationBarBackButtonHidden(true)
+        .onChange(of: selectedTab) { oldValue, newValue in
+            if newValue == .terrarium && !hasShownTerrariumPopup {
+                isTerrariumPopupVisible = true
+                hasShownTerrariumPopup = true
+            }
+        }
     }
 }
 
