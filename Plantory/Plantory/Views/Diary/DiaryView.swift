f5415702f8e828961557a97f6573e0c4ace3d41b
jiwookim1202 <godblessjiwoo@gmail.com>
2025-08-06 20:22:25 +0900

    design: SleepStepView 수정
    wheel 위아래 초록 구분선 없애고, 선택된 시간 초록색으로 뜨도록 표시

--- Plantory/Plantory/Views/Diary/DiaryView.swift
+++ Plantory/Plantory/Views/Diary/DiaryView.swift
@@ -21,6 +21,9 @@ struct MyDateFormatter {
 
 struct DiaryView: View {
     @Bindable var viewModel: StepIndicatorViewModel
+    //맨 처음 화면 날짜 선택을 위한 날ㅈ
+    @State private var selectedDate: Date = Date()
+        @State private var showFullCalendar: Bool = false
 
     var body: some View {
         ZStack(alignment: .top) {
@@ -415,125 +418,108 @@ struct PhotoStepView: View {
 
 struct SleepStepView: View {
     @State private var wakeHour: Int = 9
-        @State private var wakeMinute: Int = 0
-        @State private var wakePeriod: String = "AM"
+    @State private var wakeMinute: Int = 0
+    @State private var wakePeriod: String = "AM"
 
-        @State private var sleepHour: Int = 11
-        @State private var sleepMinute: Int = 59
-        @State private var sleepPeriod: String = "PM"
+    @State private var sleepHour: Int = 11
+    @State private var sleepMinute: Int = 59
+    @State private var sleepPeriod: String = "PM"
 
-        private let hours = Array(1...12)
-        private let minutes = Array(0...59)
-        private let periods = ["AM", "PM"]
+    private let hours = Array(1...12)
+    private let minutes = Array(0...59)
+    private let periods = ["AM", "PM"]
 
-    
     var body: some View {
         VStack(spacing: 40) {
             Text("하루의 시작과 마무리를 기록해보세요!")
                 .font(.pretendardSemiBold(20))
                 .foregroundColor(.diaryfont)
-            
+
+            // MARK: - 기상
             VStack(spacing: 12) {
                 Text("기상")
                     .font(.pretendardSemiBold(20))
                     .foregroundStyle(.diaryfont)
 
-
-                HStack(spacing: 0) {
-                    Picker("", selection: $wakeHour) {
-                        ForEach(hours, id: \.self) {
-                            Text("\($0)")
-                                .foregroundStyle(.diaryfont)
-                                
-
-
+                ZStack {
+                    HStack(spacing: 0) {
+                        Picker("", selection: $wakeHour) {
+                            ForEach(hours, id: \.self) { hour in
+                                Text("\(hour)")
+                                    .foregroundStyle(hour == wakeHour ? .green04 : .diaryfont)
+                            }
                         }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 60)
-                    
+                        .pickerStyle(.wheel)
+                        .frame(width: 60)
 
-                    Text(":")
-                        .foregroundStyle(.diaryfont)
-                        
-                    Spacer()
-                        .frame(width:4)
-                    
-                    Picker("", selection: $wakeMinute) {
-                        ForEach(minutes, id: \.self) {
-                            Text(String(format: "%02d", $0))
-                                .foregroundStyle(.diaryfont)
+                        Text(":")
+                            .foregroundStyle(.diaryfont)
+                        Spacer().frame(width: 4)
 
+                        Picker("", selection: $wakeMinute) {
+                            ForEach(minutes, id: \.self) { minute in
+                                    Text(String(format: "%02d", minute))
+                                        .foregroundStyle(minute == wakeMinute ? .green04 : .diaryfont)
+                            }
                         }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 60)
-                    
+                        .pickerStyle(.wheel)
+                        .frame(width: 60)
 
-                    Picker("", selection: $wakePeriod) {
-                        ForEach(periods, id: \.self) {
-                            Text($0)
-                                .foregroundStyle(.diaryfont)
-
+                        Picker("", selection: $wakePeriod) {
+                            ForEach(periods, id: \.self) { period in
+                                Text(period)
+                                    .foregroundStyle(period == wakePeriod ? .green04 : .diaryfont)
+                            }
                         }
+                        .pickerStyle(.wheel)
+                        .frame(width: 60)
                     }
-                    .pickerStyle(.wheel)
-                    .frame(width: 60)
-                    .foregroundColor(.diaryfont)
-
+                    .font(.pretendardSemiBold(18))
                 }
-                .font(.pretendardSemiBold(18))
-                
-
             }
-            
+
+            // MARK: - 취침
             VStack(spacing: 16) {
                 Text("취침")
                     .font(.pretendardSemiBold(20))
                     .foregroundColor(.diaryfont)
 
-                HStack(spacing: 0) {
-                    Picker("", selection: $sleepHour) {
-                        ForEach(hours, id: \.self) {
-                            Text("\($0)")
-                                .foregroundStyle(.diaryfont)
-
+                ZStack {
+                    HStack(spacing: 0) {
+                        Picker("", selection: $sleepHour) {
+                            ForEach(hours, id: \.self) { hour in
+                                Text("\(hour)")
+                                    .foregroundStyle(hour == sleepHour ? .green04 : .diaryfont)
+                            }
                         }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 60)
-                    
+                        .pickerStyle(.wheel)
+                        .frame(width: 60)
 
-                    
-                    Text(":")
-                        .foregroundStyle(.diaryfont)
-                        
-                    Spacer()
-                        .frame(width:4)
-                    
-                    Picker("", selection: $sleepMinute) {
-                        ForEach(minutes, id: \.self) {
-                            Text(String(format: "%02d", $0))
-                                .foregroundStyle(.diaryfont)
+                        Text(":")
+                            .foregroundStyle(.diaryfont)
+                        Spacer().frame(width: 4)
 
+                        Picker("", selection: $sleepMinute) {
+                            ForEach(minutes, id: \.self) { minute in
+                                Text(String(format: "%02d", minute))
+                                    .foregroundStyle(minute == sleepMinute ? .green04 : .diaryfont)
+                            }
                         }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 60)
-                    
-                    Picker("", selection: $sleepPeriod) {
-                        ForEach(periods, id: \.self) {
-                            Text($0)
-                                .foregroundStyle(.diaryfont)
+                        .pickerStyle(.wheel)
+                        .frame(width: 60)
+
+                        Picker("", selection: $sleepPeriod) {
+                            ForEach(periods, id: \.self) { period in
+                                Text(period)
+                                    .foregroundStyle(period == sleepPeriod ? .green04 : .diaryfont)
+                            }
                         }
+                        .pickerStyle(.wheel)
+                        .frame(width: 60)
                     }
-                    .pickerStyle(.wheel)
-                    .frame(width: 60)
+                    .font(.pretendardSemiBold(18))
                 }
-                .font(.pretendardSemiBold(18))
-                .foregroundColor(.diaryfont)
             }
-            
             Spacer()
         }
         .padding()
@@ -599,15 +585,6 @@ struct CompletedView: View {
                         
                         
                     }
-                    
-                    
-
-                    //일단 링크로 연결
-                    NavigationLink(
-                        destination: TerrariumView(),
-                        isActive: $isNavigatingToTerrarium,
-                        label: { EmptyView() }
-                    )
                 }
             }
         }
