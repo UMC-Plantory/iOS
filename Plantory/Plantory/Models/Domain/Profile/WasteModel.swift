8531a8c7c0fe71f53c6b9e48d80988367314d8d3
이효주 <julie08051@naver.com>
2025-08-15 04:14:30 +0900

    Feat: 휴지통 API

--- Plantory/Plantory/Models/Domain/Profile/WasteModel.swift
+++ Plantory/Plantory/Models/Domain/Profile/WasteModel.swift
@@ -2,43 +2,8 @@ import Foundation
 
 // Server Response for 휴지통 목록
 struct WasteResponse: Codable {
-    let isSuccess: Bool
-    let code: Int
-    let message: String
     let diaries: [Diary]
-
-    private enum CodingKeys: String, CodingKey {
-        case isSuccess, code, message, result
-    }
-    private enum ResultKeys: String, CodingKey {
-        case diaries
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        isSuccess = try container.decode(Bool.self, forKey: .isSuccess)
-        code      = try container.decode(Int.self, forKey: .code)
-        message   = try container.decode(String.self, forKey: .message)
-
-        let resultContainer = try container.nestedContainer(
-            keyedBy: ResultKeys.self,
-            forKey: .result
-        )
-        diaries = try resultContainer.decode([Diary].self, forKey: .diaries)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(isSuccess, forKey: .isSuccess)
-        try container.encode(code, forKey: .code)
-        try container.encode(message, forKey: .message)
-
-        var resultContainer = container.nestedContainer(keyedBy: ResultKeys.self, forKey: .result)
-        try resultContainer.encode(diaries, forKey: .diaries)
-    }
 }
-
-
 
 /// 휴지통으로 보내기!!!!! 응답 모델
 public struct WastePatchResponse: Codable {}
