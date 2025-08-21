c636022aa6265b3951a59129e7aa66e8c59cda38
minyoy <65756020+minyoy@users.noreply.github.com>
2025-08-21 15:39:51 +0900

    fix: ChatView 오류 수정

--- Plantory/Plantory/Modules/Tab/Chat/ViewModels/ChatViewModel.swift
+++ Plantory/Plantory/Modules/Tab/Chat/ViewModels/ChatViewModel.swift
@@ -9,32 +9,32 @@ import SwiftUI
 import Combine
 import Moya
 
-@Observable
-class ChatViewModel {
+@MainActor
+class ChatViewModel: ObservableObject {
     
     // MARK: - 메시지
     
     /// 화면에 띄울 메시지 목록
-    var messages: [ChatMessage] = []
+    @Published var messages: [ChatMessage] = []
     
     /// 페이지네이션을 위해, 마지막 메시지의 createdAt 저장
-    var lastCreateAt: String? = nil
+    @Published var lastCreateAt: String? = nil
     
     /// postChat 함수가 로딩 중임을 나타냄
-    var isPostingChat: Bool = false
+    @Published var isPostingChat: Bool = false
     
     /// getLatestChat 함수가 로딩 중임을 나타냄
-    var isFetchingChats: Bool = false
+    @Published var isFetchingChats: Bool = false
     
-    /// 메시지 페이지네이션에서 마지막인지를 나타냄
-    var isLast: Bool = false
+    /// 메시지 페이지네이션에서 다음 페이지가 있는지 나타냄
+    @Published var hasNext: Bool = true
     
     // 스크롤 트리거
-    var shouldScrollToBottom = false
+    @Published var shouldScrollToBottom = false
     
     // MARK: - Toast
     
-    var toast: CustomToast? = nil
+    @Published var toast: CustomToast? = nil
     
     // MARK: - 의존성 주입 및 비동기 처리
     
@@ -46,17 +46,17 @@ class ChatViewModel {
     // MARK: - 사용자 입력
     
     /// 입력된 채팅 본문
-    var textInput = ""
+    @Published var textInput = ""
     
     // MARK: - 초기화
-        
+    
     init(container: DIContainer) {
         self.container = container
     }
     
     // MARK: - 함수
     
-    public func sendMessage() {
+    public func sendMessage() async {
         let text = textInput
         self.textInput = ""
         
@@ -67,19 +67,19 @@ class ChatViewModel {
         )
         messages.append(newMessage)
         
-        self.postChat(text: text)
+        await self.postChat(text: text)
     }
     
     
     // MARK: - API
     
     /// 채팅 요청
-    public func postChat(text: String) {
+    public func postChat(text: String) async {
         guard !isPostingChat, !isFetchingChats else { return }
         isPostingChat = true
-
+        
         let request = ChatRequest(content: text)
-
+        
         container.useCaseService.chatService.postChat(chatData: request)
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { [weak self] completion in
@@ -108,8 +108,8 @@ class ChatViewModel {
     }
     
     /// 이전 대화 기록 조회에서, 커서 페이징
-    public func getChatsList() {
-        guard !isPostingChat, !isFetchingChats, !isLast else { return }
+    public func getChatsList() async {
+        guard !isPostingChat, !isFetchingChats, hasNext else { return }
         
         container.useCaseService.chatService.getChatsList(cursor: lastCreateAt)
             .receive(on: DispatchQueue.main)
@@ -124,22 +124,16 @@ class ChatViewModel {
                     self?.isFetchingChats = false
                 }
             }, receiveValue: { [weak self] response in
-                if response.chatsDetatilList.isEmpty {
-                    self?.isLast = true
-                } else {
-                    let convertedResponse = response.chatsDetatilList
-                        .map { ChatMessage(from: $0) }
-                    
-                    let reversedResponse = convertedResponse
-                        .reversed()
-                        .map { $0 }
-                    self?.messages.insert(contentsOf: reversedResponse, at: 0)
-                    
-                    /// 페이지네이션을 위해 마지막 메시지를 저장
-                    if let last = convertedResponse.last {
-                        self?.lastCreateAt = last.createdAt
-                    }
-                }
+                let convertedResponse = response.chatsDetatilList
+                    .map { ChatMessage(from: $0) }
+                
+                let reversedResponse = convertedResponse
+                    .reversed()
+                    .map { $0 }
+                self?.messages.insert(contentsOf: reversedResponse, at: 0)
+                
+                self?.lastCreateAt = response.nextCursor
+                self?.hasNext = response.hasNext
                 self?.isFetchingChats = false
             })
             .store(in: &cancellables)
--- Plantory/Plantory/Service/MoyaRouter/ChatRouter.swift
+++ Plantory/Plantory/Service/MoyaRouter/ChatRouter.swift
@@ -40,134 +40,14 @@ enum ChatRouter {
             return .requestJSONEncodable(chatData)
         case .getChatsList(let cursor):
             if let cursor = cursor {
-                return .requestParameters(parameters: ["cursor": cursor], encoding: URLEncoding.queryString)
+                return .requestParameters(parameters: ["cursor": cursor, "size": 8], encoding: URLEncoding.queryString)
             } else {
-                return .requestPlain
+                return .requestParameters(parameters: ["size": 8], encoding: URLEncoding.queryString)
             }
         }
     }
     
     var headers: [String: String]? {
         return ["Content-Type": "application/json"]
-    }
-    
-    var sampleData: Data {
-        let json: String
-        switch self {
-        case .postChat:
-            json = """
-            {
-                "isSuccess": true,
-                "code": "COMMON200",
-                "message": "성공입니다.",
-                "result": {
-                    "content": "요즘 감정의 기복이 심했던 것 같아요. 우울할 때는 혼자 감당하려 하지 말고, 주변 사람에게 살짝 기대보세요. 당신의 마음도 소중하니까요. 작은 산책이나 좋아하는 음악 듣기도 도움이 될 거예요. 힘든 마음, 천천히 다독이며 잘 돌봐주세요. 언제나 응원할게요.",
-                    "createdAt": "2025-08-12T20:25",
-                    "isMember": false
-                }
-            }
-            """
-        case .getChatsList:
-            json = """
-            {
-                "isSuccess": true,
-                "code": "COMMON200",
-                "message": "성공입니다.",
-                "result": [
-                    {
-                    "content": "우울할 때는 우선적으로 자신을 이해하고 위로해주는 것이 중요해요. 자기관리를 잘하고, 자신에게 대해 긍정적으로 생각하는 것도 좋은 방법이에요. 그리고 주변 사람들과 소통하며 좋은 에너지를 얻는 것도 도움이 될 거예요. 어떤 활동을 통해 마음을 치유하고 기분을 전환해보는 것도 좋은 방법이에요. 함께 하고 싶은 활동이 있나요? 함께 고민을 나누며 해결책을 찾아보는 것도 좋은 방법이에요. 함께해요! 😊",
-                    "createAt": "2025-07-17T17:21",
-                    "isMember": false
-                    },
-                    {
-                    "content": "우울할 때 기분이 어떻게 하면 좋아질까",
-                    "createAt": "2025-07-17T17:21",
-                    "isMember": true
-                    },
-                    {
-                    "content": "비가 오면 기분이 조금 우울해지는 것 같죠. 마음이 편하지 않으시겠어요. 함께 이야기를 나누며 마음을 털어놓아보세요. 제가 당신을 듣고 싶어요. 함께 이 감정을 헤쳐나가는 데 도움이 될 거예요. 😊",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": false
-                    },
-                    {
-                    "content": "오늘 비가 와서 좀 처지는 것 같아",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": true
-                    },
-                    {
-                    "content": "안녕하세요. 어떤 일이 있으신가요? 힘든 일이 있거나 고민거리가 있다면 말해주세요. 함께 이야기 나누며 해결해보도록 할게요. 😊",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": false
-                    },
-                    {
-                    "content": "안녕",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": true
-                    },
-                    {
-                    "content": "우울할 때는 우선적으로 자신을 이해하고 위로해주는 것이 중요해요. 자기관리를 잘하고, 자신에게 대해 긍정적으로 생각하는 것도 좋은 방법이에요. 그리고 주변 사람들과 소통하며 좋은 에너지를 얻는 것도 도움이 될 거예요. 어떤 활동을 통해 마음을 치유하고 기분을 전환해보는 것도 좋은 방법이에요. 함께 하고 싶은 활동이 있나요? 함께 고민을 나누며 해결책을 찾아보는 것도 좋은 방법이에요. 함께해요! 😊",
-                    "createAt": "2025-07-17T17:21",
-                    "isMember": false
-                    },
-                    {
-                    "content": "우울할 때 기분이 어떻게 하면 좋아질까",
-                    "createAt": "2025-07-17T17:21",
-                    "isMember": true
-                    },
-                    {
-                    "content": "비가 오면 기분이 조금 우울해지는 것 같죠. 마음이 편하지 않으시겠어요. 함께 이야기를 나누며 마음을 털어놓아보세요. 제가 당신을 듣고 싶어요. 함께 이 감정을 헤쳐나가는 데 도움이 될 거예요. 😊",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": false
-                    },
-                    {
-                    "content": "오늘 비가 와서 좀 처지는 것 같아",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": true
-                    },
-                    {
-                    "content": "안녕하세요. 어떤 일이 있으신가요? 힘든 일이 있거나 고민거리가 있다면 말해주세요. 함께 이야기 나누며 해결해보도록 할게요. 😊",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": false
-                    },
-                    {
-                    "content": "안녕",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": true
-                    },
-                    {
-                    "content": "우울할 때는 우선적으로 자신을 이해하고 위로해주는 것이 중요해요. 자기관리를 잘하고, 자신에게 대해 긍정적으로 생각하는 것도 좋은 방법이에요. 그리고 주변 사람들과 소통하며 좋은 에너지를 얻는 것도 도움이 될 거예요. 어떤 활동을 통해 마음을 치유하고 기분을 전환해보는 것도 좋은 방법이에요. 함께 하고 싶은 활동이 있나요? 함께 고민을 나누며 해결책을 찾아보는 것도 좋은 방법이에요. 함께해요! 😊",
-                    "createAt": "2025-07-17T17:21",
-                    "isMember": false
-                    },
-                    {
-                    "content": "우울할 때 기분이 어떻게 하면 좋아질까",
-                    "createAt": "2025-07-17T17:21",
-                    "isMember": true
-                    },
-                    {
-                    "content": "비가 오면 기분이 조금 우울해지는 것 같죠. 마음이 편하지 않으시겠어요. 함께 이야기를 나누며 마음을 털어놓아보세요. 제가 당신을 듣고 싶어요. 함께 이 감정을 헤쳐나가는 데 도움이 될 거예요. 😊",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": false
-                    },
-                    {
-                    "content": "오늘 비가 와서 좀 처지는 것 같아",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": true
-                    },
-                    {
-                    "content": "안녕하세요. 어떤 일이 있으신가요? 힘든 일이 있거나 고민거리가 있다면 말해주세요. 함께 이야기 나누며 해결해보도록 할게요. 😊",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": false
-                    },
-                    {
-                    "content": "안녕",
-                    "createAt": "2025-07-17T17:20",
-                    "isMember": true
-                    }
-                ]
-            }
-            """
-        }
-        return Data(json.utf8)
     }
 }
